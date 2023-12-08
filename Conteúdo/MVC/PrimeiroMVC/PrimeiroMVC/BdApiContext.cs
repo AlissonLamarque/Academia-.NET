9f07a3e93954c728af56c3c360123e155699a896
Alisson Lamarque <alissonlamarque.si@gmail.com>
2023-12-08 18:49:35 -0300

    Fundamentos de MVC

---
+++ Conteúdo/MVC/PrimeiroMVC/PrimeiroMVC/BdApiContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PrimeiroMVC.Models;
+
+namespace PrimeiroMVC;
+
+public partial class BdApiContext : DbContext
+{
+    public BdApiContext()
+    {
+    }
+
+    public BdApiContext(DbContextOptions<BdApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Email> Emails { get; set; }
+
+    public virtual DbSet<Pessoa> Pessoas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=BdApi; User ID=aulaentity; password=senha_1234; language=Portuguese;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Email>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Email__3213E83F8C788C87");
+
+            entity.ToTable("Email");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Email1)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.FkPessoa).HasColumnName("fk_pessoa");
+
+            entity.HasOne(d => d.FkPessoaNavigation).WithMany(p => p.Emails)
+                .HasForeignKey(d => d.FkPessoa)
+                .HasConstraintName("FK__Email__fk_pessoa__398D8EEE");
+        });
+
+        modelBuilder.Entity<Pessoa>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Pessoa__3213E83F281C361C");
+
+            entity.ToTable("Pessoa");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nome");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Conteúdo/MVC/PrimeiroMVC/PrimeiroMVC/Controllers/OlaMundoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace PrimeiroMVC.Controllers
+{
+    public class OlaMundoController : Controller
+    {
+        //public string index()
+        public IActionResult Index()
+        {
+            // As rotas do MVC indicam que este index é chamado automaticamente quando eu acessar a URL/OlaMundo
+            return View();
+            //return "Olá mundo";
+        }
+
+        public string Welcome(int id,  string nome)
+        {
+            //as rotas do MVC indicam que este index é chamado automaticamente quando eu acessar a URL/OlaMundo/Welcome
+            return HtmlEncoder.Default.Encode($"Oi {nome} | id: {id}");
+        }
+
+        public IActionResult MostrarMensagem(string nome, int qtd)
+        {
+            ViewData["Nome"] = nome;
+            ViewData["Qtd"] = qtd;
+
+            return View();
+        }
+    }
+}
